7d6a9a0d1b6996ed8ca86b97c66f95353cb54c9c
Tanner Rusk <tdrusk@iastate.edu>
2019-04-29 14:44:47 +0200

    main

--- BEM_Main.py
+++ BEM_Main.py
@@ -11,11 +11,11 @@ from BEM_Code import solveStreamtube as SS
 import pandas as pd
 
 #Setup
-N_ann_reg = 500          #Number of annuli
-N_ann_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9] #[ 10, 100, 200, 300, 500, 800]  [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Dis_method = 'constant'   #Discretisation method, ('cosine' or 'constant')
+N_ann_reg = 900              #Number of annuli
+N_ann_list = [1, 5, 10, 50, 250, 500, 750, 800, 950]
+Dis_method = 'cosine'   #Discretisation method, ('cosine' or 'constant')
 case = 'turbine'        #'turbine' or 'propeller'
-convergence_test = False
+convergence_test = True
 
 
 #Import correct geometry file
@@ -24,7 +24,7 @@ if case == 'turbine':
     print('Turbine')
 elif case == 'propeller':
     import BEM_Geometry_propeller as V
-    print('Propeller')
+    print('Propellor')
 else:
     print('You are DOOMED')
 
@@ -41,12 +41,12 @@ if convergence_test:
         N_ann = int(N_ann)
         print(N_ann)
         if Dis_method == 'cosine':
-            r_R = np.linspace(V.rootradius_R,1,N_ann+1)
+            r_R = np.linspace(0,1,N_ann+1)
             for n in range(1,N_ann+2):
                 theta = np.pi/N_ann * (n-1)
                 r_R[n-1] = V.rootradius_R+((0.5-V.rootradius_R/2)-(0.5-V.rootradius_R/2)*np.cos(theta))
         elif Dis_method == 'constant':
-            r_R = np.linspace(V.rootradius_R,1,N_ann+1)
+            r_R = np.linspace(0.2,1,N_ann+1)
         
         
         #Calculation
@@ -63,7 +63,6 @@ if convergence_test:
             sheet   = "Sheet1"
             chord_distribution = 0.18-0.06*r_R # meters
             twist_distribution = -50*r_R+V.Pblade # degrees
-            
         
         
         data1=pd.read_excel( airfoil, sheet)#airfoil, header=0,
@@ -72,15 +71,15 @@ if convergence_test:
         polar_cl = data1['Cl']#[:]
         polar_cd = data1['Cd']#[:]
         
-        results = np.zeros((N_ann,14))                                    #***CHANGE***
-        r_R_centroid = np.zeros((N_ann,1))                                #***CHANGE***
+        results = np.zeros((N_ann,13))                                     #***CHANGE***
+        r_R_centroid = np.zeros((N_ann,6))                                #***CHANGE***
         for annulus in range(0,N_ann):                                    #***CHANGE***
             r1_R = r_R[annulus]                                           #***CHANGE***
             r2_R = r_R[annulus+1]                                         #***CHANGE***
             r_R_centroid[annulus] = (r1_R + r2_R)/2                       #***CHANGE***              
             chord = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R.transpose(), chord_distribution.transpose())#***CHANGE***
             twist = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R, twist_distribution)                        #***CHANGE***
-            results[annulus,:] = SS(case, V.rho, V.U0, r1_R, r2_R, V.rootradius_R, V.tipradius_R, Omega, V.R, V.Nblades, chord, twist, polar_alpha, polar_cl, polar_cd, V.TSR)
+            results[annulus,:] = SS(case, V.rho, V.U0, r1_R, r2_R, V.rootradius_R, V.tipradius_R, Omega, V.R, V.Nblades, chord, twist, polar_alpha, polar_cl, polar_cd)
         
         areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*V.R**2
         dr = (r_R[1:]-r_R[:-1])*V.R
@@ -92,62 +91,55 @@ if convergence_test:
 #----------------------------------------
 #Main Block
 #----------------------------------------
-        
-elif convergence_test==False:
+if Dis_method == 'cosine':
+    r_R = np.linspace(0,1,N_ann_reg+1)
+    for n in range(1,N_ann_reg+2):
+        theta = np.pi/N_ann_reg * (n-1)
+        r_R[n-1] = V.rootradius_R+((0.5-V.rootradius_R/2)-(0.5-V.rootradius_R/2)*np.cos(theta))
+elif Dis_method == 'constant':
+    r_R = np.linspace(0.2,1,N_ann_reg+1)
+
+
+#Calculation
+if case == 'turbine':
+    Omega =V.U0*V.TSR/V.R
+    airfoil = 'polar_DU95W180.xlsx' # Where are we using this ?
+    sheet   = "Blade1"
+    chord_distribution = 3*(1-r_R)+1 # meters
+    twist_distribution = -14*(1-r_R)+V.Pblade # degrees !!SEND QUESTION ABOUT -14 or +14
     
-    if Dis_method == 'cosine':
-        r_R = np.linspace(V.rootradius_R,1,N_ann_reg+1)
-        for n in range(1,N_ann_reg+2):
-            theta = np.pi/N_ann_reg * (n-1)
-            r_R[n-1] = V.rootradius_R+((0.5-V.rootradius_R/2)-(0.5-V.rootradius_R/2)*np.cos(theta))
-    elif Dis_method == 'constant':
-        r_R = np.linspace(V.rootradius_R,1,N_ann_reg+1)
+elif case == 'propeller':
+    Omega = V.rpm/60*2*np.pi
+    airfoil = 'ARA_airfoil_data.xlsx'
+    sheet   = "Sheet1"
+    chord_distribution = 0.18-0.06*r_R # meters
+    twist_distribution = -50*r_R+V.Pblade # degrees
 
 
-    #Calculation
-    if case == 'turbine':
-        Omega =V.U0*V.TSR/V.R
-        airfoil = 'polar_DU95W180.xlsx' # Where are we using this ?
-        sheet   = "Blade1"
-        chord_distribution = 3*(1-r_R)+1 # meters
-        twist_distribution = -14*(1-r_R)+V.Pblade # degrees !!SEND QUESTION ABOUT -14 or +14
-    
-    elif case == 'propeller':
-        Omega = V.rpm/60*2*np.pi
-        airfoil = 'ARA_airfoil_data.xlsx'
-        sheet   = "Sheet1"
-        chord_distribution = 0.18-0.06*r_R # meters
-        twist_distribution = -50*r_R+V.Pblade # degrees
-
-
-    data1=pd.read_excel( airfoil, sheet)#airfoil, header=0,
+data1=pd.read_excel( airfoil, sheet)#airfoil, header=0,
                     #names = ["Alfa", "Cl", "Cd", "Cm"], sep='\s+')
-    polar_alpha = data1['Alfa']#[:]
-    polar_cl = data1['Cl']#[:]
-    polar_cd = data1['Cd']#[:]
+polar_alpha = data1['Alfa']#[:]
+polar_cl = data1['Cl']#[:]
+polar_cd = data1['Cd']#[:]
 
-    results = np.zeros((N_ann_reg,14))                                     #***CHANGE***
-    r_R_centroid = np.zeros((N_ann_reg,1))                                #***CHANGE***
-    for annulus in range(0,N_ann_reg):                                    #***CHANGE***
-        r1_R = r_R[annulus]                                           #***CHANGE***
-        r2_R = r_R[annulus+1]                                         #***CHANGE***
-        r_R_centroid[annulus] = (r1_R + r2_R)/2                       #***CHANGE***              
-        chord = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R.transpose(), chord_distribution.transpose())#***CHANGE***
-        twist = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R, twist_distribution)                        #***CHANGE***
-        results[annulus,:] = SS(case, V.rho, V.U0, r1_R, r2_R, V.rootradius_R, V.tipradius_R, Omega, V.R, V.Nblades, chord, twist, polar_alpha, polar_cl, polar_cd, V.TSR)
+results = np.zeros((N_ann_reg,13))                                     #***CHANGE***
+r_R_centroid = np.zeros((N_ann_reg,6))                                #***CHANGE***
+for annulus in range(0,N_ann_reg):                                    #***CHANGE***
+    r1_R = r_R[annulus]                                           #***CHANGE***
+    r2_R = r_R[annulus+1]                                         #***CHANGE***
+    r_R_centroid[annulus] = (r1_R + r2_R)/2                       #***CHANGE***              
+    chord = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R.transpose(), chord_distribution.transpose())#***CHANGE***
+    twist = np.interp((r_R[annulus]+r_R[annulus+1])/2, r_R, twist_distribution)                        #***CHANGE***
+    results[annulus,:] = SS(case, V.rho, V.U0, r1_R, r2_R, V.rootradius_R, V.tipradius_R, Omega, V.R, V.Nblades, chord, twist, polar_alpha, polar_cl, polar_cd)
 
-    #Plot results
-    areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*V.R**2
-    dr = (r_R[1:]-r_R[:-1])*V.R
-    CT = np.sum(dr*results[:,3]*V.Nblades/(0.5*V.U0**2*V.rho**np.pi*V.R**2))
-    CP = np.sum(dr*results[:,4]*results[:,2]*V.Nblades*V.R*Omega/(0.5*V.U0**3*V.rho*np.pi*V.R**2))
-    
-    print('CP is ', CP)
-    print('CT is ', CT)
+#Plot results
+areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*V.R**2
+dr = (r_R[1:]-r_R[:-1])*V.R
+CT = np.sum(dr*results[:,3]*V.Nblades/(0.5*V.U0**2*V.rho**np.pi*V.R**2))
+CP = np.sum(dr*results[:,4]*results[:,2]*V.Nblades*V.R*Omega/(0.5*V.U0**3*V.rho*np.pi*V.R**2))
 
-
-#-----------------------------------------------------------------------------
-#***PLOT***#
+print('CP is ', CP)
+print('CT is ', CT)
     
 #Axial and tangential induction factor over radius
 fig_a = plt.figure(figsize=(12,6))
@@ -198,25 +190,18 @@ plt.xlabel('r/R')
 plt.legend()
 plt.show()
 
-##Ct and Cn distribution
-#fig_ct_cn = plt.figure(figsize=(12,6))
-#plt.title(r'Distribution of $C_{n}$ and $C_{t}$')
-#plt.plot(results[:,2], results[:,9], 'r-' , label=r'$C_{n}$')#***CHANGE***
-#plt.plot(results[:,2], results[:,8], 'g--', label=r'$C_{t}$')
-#plt.grid()
-#plt.xlabel('r/R')
-#plt.legend()
-#plt.show()
-
-#Cq distribution (no clue)
+#Ct and Cn distribution
 fig_ct_cn = plt.figure(figsize=(12,6))
-plt.title(r'Distribution of $C_{Q}$')
-plt.plot(results[:,2], results[:,13], 'r-' , label=r'$C_{n}$')#***CHANGE***
-#plt.plot(results[:,2], results[:,8], 'g--', label=r'$C_{t}$')
+plt.title(r'Distribution of $C_{n}$ and $C_{t}$')
+plt.plot(results[:,2], results[:,9], 'r-' , label=r'$C_{n}$')#***CHANGE***
+plt.plot(results[:,2], results[:,8], 'g--', label=r'$C_{t}$')
 plt.grid()
 plt.xlabel('r/R')
 plt.legend()
 plt.show()
+
+#Cq distribution (no clue)
+
 
 #Prandtl correction
 fig_prandtl = plt.figure(figsize=(12,6))
@@ -232,25 +217,11 @@ plt.show()
 #Convergence plot
 if convergence_test:
     fig_prandtl = plt.figure(figsize=(12,6))
-    plt.title(r'Convergence of $C_{p}$')
+    plt.title(r'Convergence of C_{t} and $C_{p}$')
     plt.grid()
-    #plt.yscale('log')
-    plt.plot(N_ann_list, CP_conv, 'g--', label=r'C_{p}')
-    #plt.plot(N_ann_list, CT_conv, 'r-', label=r'C_{t}')
-    #plt.plot(N_ann_list[1:-1], abs(CP_conv[-1] - CP_conv[1:-1]), 'g--', label=r'C_{p}')
-#    plt.plot(N_ann_list[1:-1], abs(CT_conv[-1] - CT_conv[1:-1]), 'r-', label=r'C_{t}')
-    plt.xlabel('# of annuli')
-    plt.legend()
-    plt.show()
-    
-    fig_prandtl = plt.figure(figsize=(12,6))
-    plt.title(r'Convergence of $C_{t}$')
-    plt.grid()
-    #plt.yscale('log')
-    #plt.plot(N_ann_list, CP_conv, 'g--', label=r'C_{p}')
-    plt.plot(N_ann_list, CT_conv, 'r-', label=r'C_{t}')
-#    plt.plot(N_ann_list[1:-1], abs(CP_conv[-1] - CP_conv[1:-1]), 'g--', label=r'C_{p}')
-    #plt.plot(N_ann_list[1:-1], abs(CT_conv[-1] - CT_conv[1:-1]), 'r-', label=r'C_{t}')
+    plt.yscale('log')
+    plt.plot(N_ann_list[1:-1], abs(CP_conv[-1] - CP_conv[1:-1]), 'g--', label=r'C_{p}')
+    plt.plot(N_ann_list[1:-1], abs(CT_conv[-1] - CT_conv[1:-1]), 'r-', label=r'C_{t}')
     plt.xlabel('# of annuli')
     plt.legend()
     plt.show()
