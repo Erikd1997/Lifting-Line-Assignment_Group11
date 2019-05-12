54b528971ddf905b6397c5f691a2f99cc6b2fe4a
Erik Dedding <erik.dedding@hotmail.nl>
2019-05-12 18:23:47 +0200

    Updated vortex rings
    Its producing rings, but there is still a small bug in there

--- Lifting-Line_Rings.py
+++ Lifting-Line_Rings.py
@@ -11,14 +11,14 @@ def WakeGeometry(U_infty, omega, n_t, n_r, a_w, NBlades, R, chord, plot=False):
     U_w = U_infty*(1-a_w)
     
     #Blade geometry distribution
-    r_U = R[1:].reshape([len(R)-1, 1, 1, 1])
-    r_B = R[:-1].reshape([len(R)-1, 1, 1, 1])
-    chord_U = chord[1:].reshape([len(R)-1, 1, 1, 1])
-    chord_B = chord[:-1].reshape([len(R)-1, 1, 1, 1])
+    r_U = R[1:].reshape([len(R)-1, 1, 1])
+    r_B = R[:-1].reshape([len(R)-1, 1, 1])
+    chord_U = chord[1:].reshape([len(R)-1, 1, 1])
+    chord_B = chord[:-1].reshape([len(R)-1, 1, 1])
     
     #Blade number
     Blades = np.arange(NBlades)
-    Blades = Blades.reshape([1, 1, NBlades, 1])
+    Blades = Blades.reshape([1, 1, NBlades])
     theta_0 = (Blades) * 2*np.pi/NBlades
     
     #Setup time array with extra zeros in middle
@@ -27,32 +27,32 @@ def WakeGeometry(U_infty, omega, n_t, n_r, a_w, NBlades, R, chord, plot=False):
     t_int = list(t_int)
     t_int.insert(0, 0)
     t = np.array(t_int)
-    t = t.reshape([1, n_t, 1, 1])
+    t = t.reshape([1, n_t+1, 1])
     
     #Calculate 4-dimensional matrix
-    zero_offset = np.zeros([len(chord_U), 1, 1, 1])
+    zero_offset = np.zeros([len(chord_U), 1, 1])
     
-    offset_U_int = n_t*[chord_U]
+    offset_U_int = (n_t)*[chord_U]
     offset_U = np.array(offset_U_int)
-    offset_U = offset_U.reshape([len(chord_U), n_t, 1, 1])
-    offset_U = np.concatenate((zero_offset,offset_U))
+    offset_U = offset_U.reshape([len(chord_U), n_t, 1])
+    offset_U = np.concatenate((zero_offset,offset_U),axis=1)
     
-    offset_B_int = n_t*[chord_B]
+    offset_B_int = (n_t)*[chord_B]
     offset_B = np.array(offset_B_int)
-    offset_B = offset_B.reshape([len(chord_B), n_t, 1, 1])
-    offset_B = np.concatenate((zero_offset,offset_B))
+    offset_B = offset_B.reshape([len(chord_B), n_t, 1])
+    offset_B = np.concatenate((zero_offset,offset_B),axis=1)
     
     xw_U = t*U_w + offset_U
     yw_U = -r_U*np.sin(omega*t+theta_0)
     zw_U = r_U*np.cos(omega*t+theta_0)
     
-    xw_B = np.fliplr(t*U_w + offset_B)
-    yw_B = np.fliplr(-r_B*np.sin(omega*t+theta_0))
-    zw_B = np.fliplr(r_B*np.cos(omega*t+theta_0))
+    xw_B = np.fliplr(t)*U_w + offset_B
+    yw_B = -r_B*np.sin(omega*np.fliplr(t)+theta_0)
+    zw_B = r_B*np.cos(omega*np.fliplr(t)+theta_0)
     
-    xw = np.transpose(np.hstack((xw_B, xw_U)))
-    yw = np.transpose(np.hstack((yw_B, yw_U)))
-    zw = np.transpose(np.hstack((zw_B, zw_U)))
+    xw = np.concatenate((xw_B, xw_U), axis=1)
+    yw = np.concatenate((yw_B, yw_U), axis=1)
+    zw = np.concatenate((zw_B, zw_U), axis=1)
             
     Rings[:,:,:,0] = xw
     Rings[:,:,:,1] = yw
@@ -60,20 +60,22 @@ def WakeGeometry(U_infty, omega, n_t, n_r, a_w, NBlades, R, chord, plot=False):
     if plot:
         fig = plt.figure()
         ax = fig.gca(projection='3d')
+#        ax.plot(Rings[40,:,0,0], Rings[40,:,0,1], Rings[40,:,0,2], 'b-')
+
         for Blade in range(NBlades):
-            for Radial_Point in range(len(r_U)):
-                ax.plot(Rings[Radial_Point,:,Blade,0], Rings[Radial_Point,:,Blade,1], Rings[Radial_Point,:,Blade,2], 'b-')
+            for Radial_Point in range(0,len(r_U),10):
+                ax.plot(Rings[Radial_Point,:n_t,Blade,0], Rings[Radial_Point,:n_t,Blade,1], Rings[Radial_Point,:n_t,Blade,2], 'b-')
                 
         plt.show()
     return Rings
 
 #Defined elsewhere
 omega = 10
-U_infty = 10
+U_infty = 100
 
 #Define input parameters
-n_t = 50
-n_r = 10
+n_t = 400
+n_r = 1
 a_w = 0.2
 
 NBlades = 3
